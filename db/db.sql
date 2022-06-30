9e14811d94329787589abc57d7f30098a8e3ff57
S-K-Sami <sami8907456312@gmail.com>
2022-06-30 17:17:38 +0600

    ARRANGE post.js

--- db/db.sql
+++ db/db.sql
@@ -2,7 +2,6 @@ DROP DATABASE db_project;
 CREATE DATABASE db_project;
 USE db_project;
 
--- TABLES
 CREATE TABLE expenditure(
     purchase_id INT AUTO_INCREMENT,
     item_name VARCHAR(255),
@@ -12,16 +11,14 @@ CREATE TABLE expenditure(
     per_unit_price INT,
     _provider VARCHAR(255),
     total_price INT,
-
     PRIMARY KEY (purchase_id)
 );
 CREATE TABLE inventory(
     item_id INT AUTO_INCREMENT,
-	  item_name VARCHAR(255),
+    item_name VARCHAR(255),
     quantity INT,
     _provider VARCHAR(255),
     _location VARCHAR(255),
-    
     PRIMARY KEY (item_id)
 );
 CREATE TABLE catalogue(
@@ -31,13 +28,11 @@ CREATE TABLE catalogue(
     quantity INT,
     price INT,
     category VARCHAR(255),
-    
     PRIMARY KEY (dish_name)
 );
 CREATE TABLE ingredient(
     dish_name VARCHAR(255),
     ingredient_name VARCHAR(255),
-    
     FOREIGN KEY (dish_name) REFERENCES catalogue(dish_name)
 );
 CREATE TABLE _order(
@@ -46,29 +41,25 @@ CREATE TABLE _order(
     total_price INT,
     _time TIME,
     _date DATE,
-    
     PRIMARY KEY (order_id)
 );
 CREATE TABLE dishes(
     order_id INT,
     dish_name VARCHAR(255),
-    
     FOREIGN KEY (order_id) REFERENCES _order(order_id)
 );
 CREATE TABLE reservation(
     reservation_id INT AUTO_INCREMENT,
     customer_id VARCHAR(255),
-    _table INT,
+    table_no INT,
     no_of_guest INT,
-    
     PRIMARY KEY (reservation_id)
 );
 CREATE TABLE customer(
     customer_id INT,
-    name VARCHAR(255),
+    _name VARCHAR(255),
     contact VARCHAR(255),
-    address VARCHAR(255),
-    
+    _address VARCHAR(255),
     PRIMARY KEY (customer_id)
 );
 CREATE TABLE bill(
@@ -79,22 +70,19 @@ CREATE TABLE bill(
     payment_returned INT,
     _date Date,
     _time Time,
-    
     PRIMARY KEY(bill_id)
 );
 CREATE TABLE item(
     bill_id INT,
     item_name VARCHAR(255),
-    
     FOREIGN KEY (bill_id) REFERENCES bill(bill_id)
 );
 CREATE TABLE employee(
     employee_id INT,
     employee_name VARCHAR(255),
-    role VARCHAR(255),
+    _role VARCHAR(255),
     salary INT,
     supervisor VARCHAR(255),
-    
     PRIMARY KEY(employee_id)
 );
 CREATE TABLE emp_schedule(
@@ -103,38 +91,32 @@ CREATE TABLE emp_schedule(
     start_time TIME,
     end_time TIME,
     _date DATE,
-    
     PRIMARY KEY (schedule_id),
     FOREIGN KEY (employee_id) REFERENCES employee(employee_id)
 );
 
--- RELATIONS
 CREATE TABLE populates(
     purchase_id INT,
     item_id INT,
-    
     FOREIGN KEY (purchase_id) REFERENCES expenditure(purchase_id),
     FOREIGN KEY (item_id) REFERENCES inventory(item_id)
 );
 CREATE TABLE dictates(
     item_id INT,
     dish_name VARCHAR(255),
-    
     FOREIGN KEY (item_id) REFERENCES inventory(item_id),
-    FOREIGN KEY  (dish_name) REFERENCES catalogue(dish_name)
+    FOREIGN KEY (dish_name) REFERENCES catalogue(dish_name)
 );
 CREATE TABLE _from(
     dish_name VARCHAR(255),
     order_id INT NOT NULL,
     quantity INT,
-    
     FOREIGN KEY (dish_name) REFERENCES catalogue(dish_name),
     FOREIGN KEY (order_id) REFERENCES _order(order_id)
 );
 CREATE TABLE gives(
     order_id INT,
     reservation_id INT,
-    
     FOREIGN KEY (order_id) REFERENCES _order(order_id),
     FOREIGN KEY (reservation_id) REFERENCES reservation(reservation_id)
 );
@@ -143,35 +125,32 @@ CREATE TABLE reserves(
     customer_id INT,
     _time TIME,
     _date DATE,
-    
     FOREIGN KEY (reservation_id) REFERENCES reservation(reservation_id),
     FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
 );
 CREATE TABLE pays(
     customer_id INT,
     bill_id INT,
-    
     FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
     FOREIGN KEY (bill_id) REFERENCES bill(bill_id)
 );
 CREATE TABLE incurs(
     order_id INT,
     bill_id INT,
-    
     FOREIGN KEY (order_id) REFERENCES _order(order_id),
     FOREIGN KEY (bill_id) REFERENCES bill(bill_id)
 );
 CREATE TABLE requires(
     reservation_id INT,
     schedule_id INT,
-    
     FOREIGN KEY (reservation_id) REFERENCES reservation(reservation_id),
     FOREIGN KEY (schedule_id) REFERENCES emp_schedule(schedule_id)
 );
 CREATE TABLE works(
     employee_id INT,
     schedule_id INT,
-    
     FOREIGN KEY (employee_id) REFERENCES employee(employee_id),
     FOREIGN KEY (schedule_id) REFERENCES emp_schedule(schedule_id)
 );
+
+SELECT * FROM expenditure;
--- routers/post.js
+++ routers/post.js
@@ -1,117 +1,125 @@
-const express = require('express')
-const db = require('../db/db')
+const express = require('express');
+const db = require('../db/db');
 
 
 const router = express.Router();
 
 
-router.post('/employee',(req,res) =>{
-    const sql = `INSERT INTO employee (employee_id,employee_name,role,salary,supervisor) VALUES (${req.body.id},"${req.body.name}","${req.body.role}",${req.body.salary},"${req.body.supervisor}")`
+router.post('/expenditure', (req, res) => {
+    const sql = `INSERT INTO expenditure (item_name,_type,quantity,_date,per_unit_price,_provider,total_price) 
+    VALUES ("${req.body.item_name}","${req.body._type}","${req.body.quantity}","${req.body._date}","${req.body.per_unit_price}","${req.body._provider}",${req.body.quantity * req.body.per_unit_price})`;
 
-    db.query(sql,(err,result) =>{
-        if(err) throw err;
+    db.query(sql, (err, result) => {
+        if (err) throw err;
         res.send(result);
-    })
-})
+    });
+});
 
-router.post('/order',(req,res) => {
-    const sql =  `INSERT INTO _order ( order_id, table_no, total_price, _time, _date) VALUES (${req.body.order_id},${req.body.table_no},${req.body.total_price},"${req.body._time}","${req.body._date}")`
 
-    db.query(sql, (err,result) => {
-        if(err) throw err; 
-        res.send(result)
-    })
-})
+router.post('/inventory', (req, res) => {
+    const sql = `INSERT INTO inventory (item_name,quantity,_provider,_location) VALUES ("${req.body.item_name}",${req.body.quantity},"${req.body._provider}","${req.body._location}")`;
 
-router.post('/bill',(req,res) =>{
-    let sql = `INSERT INTO bill ( bill_id,customer_id,payment_total,payment_paid,payment_returned, _date,  _time ) VALUES (${req.body.bill_id},${req.body.customer_id},${req.body.payment_total},${req.body.payment_paid},${req.body.payment_returned},"${req.body._date}","${req.body._time}");`
-    req.body.item.map((value, idx) => {
-        sql += `INSERT INTO item ( bill_id, item_name ) VALUES (${req.body.bill_id},"${value}");`;
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
+});
+
+
+router.post('/catalogue', (req, res) => {
+    const sql = `INSERT INTO catalogue ( dish_name, genre, cook_item, quantity, price, category) 
+    VALUES (" ${req.body.dish_name} "," ${req.body.genre} "," ${req.body.cook_item} "," ${req.body.quantity} "," ${req.body.price} "," ${req.body.category} ")`;
+
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send(result);
     });
 
-    db.query(sql, (err,result) => {
-        if(err) throw err; 
-        res.send(result)
-    })
-})
-
-router.post('/catalogue',(req,res) =>{
-    const sql = `INSERT INTO catalogue ( dish_name, genre, cook_item, quantity, price, category) 
-    VALUES (" ${req.body.dish_name} "," ${req.body.genre} "," ${req.body.cook_item} "," ${req.body.quantity} "," ${req.body.price} "," ${req.body.category} ")`
-
-    db.query(sql, (err,result) => {
-        if(err) throw err; 
-        res.send(result)
-    })
-})
+    req.body.ingredient.forEach((value) => {
+        sql = `INSERT INTO ingredient ( dish_name, ingredient_name ) VALUES (${req.body.dish_name},"${value}");`;
+        db.query(sql, (err, result) => {
+            if (err) throw err;
+            res.send(result);
+        });
+    });
+});
 
 
-router.post('/customer',(req,res) =>{
-    const sql = `INSERT INTO customer ( customer_id,name,contact,address) VALUES (${req.body.customer_id},"${req.body.name}","${req.body.contact}", "${req.body.address}")`
+router.post('/order', (req, res) => {
+    const sql = `INSERT INTO _order ( order_id, table_no, total_price, _time, _date) VALUES (${req.body.order_id},${req.body.table_no},${req.body.total_price},"${req.body._time}","${req.body._date}")`;
 
-    db.query(sql, (err,result) => {
-        if(err) throw err; 
-        res.send(result)
-    })
-})
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
 
-
-router.post('/dishes',(req,res) =>{
-    const sql = `INSERT INTO customer (order_id,dish_name) VALUES (${req.body.order_id},"${req.body.dish_name}")`
-
-    db.query(sql, (err,result) => {
-        if(err) throw err; 
-        res.send(result)
-    })
-})
+    req.body.dishes.forEach((value) => {
+        sql = `INSERT INTO dishes ( order_id, dish_name ) VALUES (${req.body.order_id},"${value}");`;
+        db.query(sql, (err, result) => {
+            if (err) throw err;
+            res.send(result);
+        });
+    });
+});
 
 
-router.post('/expenditure',(req,res) =>{
-    const sql = `INSERT INTO expenditure (item_name,_type,quantity,_date,per_unit_price,_provider,total_price) 
-    VALUES ("${req.body.item_name}","${req.body._type}","${req.body.quantity}","${req.body._date}","${req.body.per_unit_price}","${req.body.provider}",${req.body.quantity*req.body.per_unit_price})` 
-    db.query(sql, (err,result) => {
-        if(err) throw err; 
-        res.send(result)
-    })
-})
+router.post('/reservation', (req, res) => {
+    const sql = `INSERT INTO reservation (customer_id,table_no,no_of_guest)
+    VALUES ("${req.body.customer_id}",${req.body.table_no},${req.body.no_of_guest})`;
+
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
+});
 
 
-router.post('/ingredient',(req,res) =>{
-    const sql = `INSERT INTO ingredient (dish_name,ingredient_name) VALUES ("${req.body.dish_name}","${req.body.ingredient_name}")`
+router.post('/customer', (req, res) => {
+    const sql = `INSERT INTO customer ( customer_id,_name,contact,_address) VALUES (${req.body.customer_id},"${req.body._name}","${req.body.contact}", "${req.body._address}")`;
 
-    db.query(sql, (err,result) => {
-        if(err) throw err; 
-        res.send(result)
-    })
-})
-
-
-router.post('/inventory',(req,res) =>{
-    const sql = `INSERT INTO inventory (item_name,quantity,_provider,_location) VALUES ("${req.body.item_name}",${req.body.quantity},"${req.body._provider}","${req.body._location}")`
-
-    
-    db.query(sql, (err,result) => {
-        if(err) throw err; 
-        res.send(result)
-    })
-})
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
+});
 
 
-router.post('/reservation',(req,res) =>{
-    const sql = `INSERT INTO reservation (customer_id,_table,no_of_guest)
-    VALUES ("${req.body.customer_id}",${req.body.table_no},${req.body.no_of_guest})`
-    
-    db.query(sql, (err,result) => {
-        if(err) throw err; 
-        res.send(result)
-    })
-})
+router.post('/bill', (req, res) => {
+    let sql = `INSERT INTO bill ( bill_id,customer_id,payment_total,payment_paid,payment_returned, _date,  _time ) VALUES (${req.body.bill_id},${req.body.customer_id},${req.body.payment_total},${req.body.payment_paid},${req.body.payment_returned},"${req.body._date}","${req.body._time}");`;
+
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
+
+    req.body.item.forEach((value) => {
+        sql = `INSERT INTO item ( bill_id, item_name ) VALUES (${req.body.bill_id},"${value}");`;
+        db.query(sql, (err, result) => {
+            if (err) throw err;
+            res.send(result);
+        });
+    });
+});
 
 
+router.post('/employee', (req, res) => {
+    const sql = `INSERT INTO employee (employee_id,employee_name,_role,salary,supervisor) VALUES (${req.body.employee_id},"${req.body.employee_name}","${req.body._role}",${req.body.salary},"${req.body.supervisor}")`;
+
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
+});
 
 
+router.post('/emp_schedule', (req, res) => {
+    const sql = `INSERT INTO emp_schedule (employee_id,start_time,end_time,_date) VALUES (${req.body.employee_id},"${req.body.start_time}","${req.body.end_time}","${req.body._date}")`;
 
-
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
+});
 
 
 module.exports = router;
