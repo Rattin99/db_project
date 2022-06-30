3e16a96a2c86ad6275be1240e10c8bdfaf4e6a5f
Rattin99 <rattin99m18@gmail.com>
2022-06-30 21:56:19 +0600

    inventory

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express')
 const router = require('./routers/getall')
 const postRouter = require('./routers/post');
 const menu = require('./routers/menu')
+const inventory = require('./routers/inventory') 
+
 const app = express()
 
 app.use(express.json())
 app.use('/getall/',router)
 app.use('/api/',postRouter);
 app.use('/menu/',menu);
+app.use('/inventory/',inventory)
 
 
 
