34a2409bb831191eab444cab9fcb40f4fec974fd
S-K-Sami <sami8907456312@gmail.com>
2022-08-12 20:32:50 +0600

    DELETE catalogue

--- routers/delete.js
+++ routers/delete.js
@@ -4,8 +4,14 @@ const db = require('../db/db');
 
 const router = express.Router();
 
-router.delete('/', (req,res) =>{
-    
+router.delete('/catalogue', (req,res) =>{
+    console.log(req.body);
+    const sql = `DELETE FROM catalogue where dish_name="${req.body.dishName}"`;
+
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
 })
 
 
